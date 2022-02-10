187a63ecfd45cf9b91ea717297ad52d8ed676bb8
Muhib-ur-Rahman <mbakar.itp@sparkcognition.com>
2022-02-10 16:27:01 +0500

    calling added

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import { ContextProvider } from "./SocketContext";
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <ContextProvider>
+    <App />
+  </ContextProvider>,
+  document.getElementById("root")
 );
